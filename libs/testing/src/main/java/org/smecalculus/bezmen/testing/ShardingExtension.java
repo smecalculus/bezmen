c0cf693676d8e25ba3ee03a955bfc7ebb231dc05
Pavel Vetokhin <p.vetokhin@gmail.com>
2023-04-18 21:51:49 +0300

    shard system tests

---
+++ libs/testing/src/main/java/org/smecalculus/bezmen/testing/ShardingExtension.java
@@ -0,0 +1,28 @@
+package org.smecalculus.bezmen.testing;
+
+import org.junit.jupiter.api.extension.ConditionEvaluationResult;
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import static org.junit.jupiter.api.extension.ConditionEvaluationResult.disabled;
+import static org.junit.jupiter.api.extension.ConditionEvaluationResult.enabled;
+
+public class ShardingExtension implements ExecutionCondition {
+
+    @Override
+    public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
+        int modulus = Integer.getInteger("bezmen.testing.modulus", 1);
+        if (modulus <= 1) {
+            return enabled("without sharding");
+        }
+        if (context.getTestMethod().isEmpty()) {
+            return enabled("classes always enabled");
+        }
+        var reminder = Integer.getInteger("bezmen.testing.reminder", 0);
+        var testId = context.getUniqueId().hashCode();
+        if (testId % modulus == reminder) {
+            return enabled("shard match: " + reminder);
+        }
+        return disabled("shard mismatch: " + reminder);
+    }
+}
