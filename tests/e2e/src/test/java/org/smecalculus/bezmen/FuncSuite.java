6c041fe1919c52b66c790a9c216443db48974fd1
Pavel Vetokhin <p.vetokhin@gmail.com>
2023-07-30 17:34:58 +0300

    merge e2e test modules

--- tests/e2e/src/test/java/org/smecalculus/bezmen/FuncSuite.java
+++ tests/e2e/src/test/java/org/smecalculus/bezmen/FuncSuite.java
@@ -4,5 +4,5 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages("org.smecalculus.bezmen.func")
+@SelectPackages("org.smecalculus.bezmen.registration")
 public class FuncSuite {}
