ee5996e2b66f3cfc91aed068fe4da0f79a37a79d
Pavel Vetokhin <p.vetokhin@gmail.com>
2023-09-16 22:11:13 +0300

    Test check failure

--- apps/foo/src/test/java/smecalculus/bezmen/data/impl/SepulkaDaoMyBatisTest.java
+++ apps/foo/src/test/java/smecalculus/bezmen/data/impl/SepulkaDaoMyBatisTest.java
@@ -1,11 +1,13 @@
 package smecalculus.bezmen.data.impl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class SepulkaDaoMyBatisTest {
 
     @Test
     void foo() {
-        // empty
+        assertThat(false).isEqualTo(true);
     }
 }
