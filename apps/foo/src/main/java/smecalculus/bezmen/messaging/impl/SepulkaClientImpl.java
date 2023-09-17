ce1117572e860d26970a33d6c693203152da5e77
Pavel Vetokhin <p.vetokhin@gmail.com>
2023-09-17 10:09:18 +0300

    Test fail on empty 4

--- apps/foo/src/main/java/smecalculus/bezmen/messaging/impl/SepulkaClientImpl.java
+++ apps/foo/src/main/java/smecalculus/bezmen/messaging/impl/SepulkaClientImpl.java
@@ -3,14 +3,13 @@ package smecalculus.bezmen.messaging.impl;
 import lombok.NonNull;
 import smecalculus.bezmen.domain.spec.Sepulka;
 import smecalculus.bezmen.domain.spec.SepulkaConverter;
-import smecalculus.bezmen.domain.spec.SepulkaService;
 import smecalculus.bezmen.messaging.spec.SepulkaClient;
 import smecalculus.bezmen.messaging.spec.SepulkaRegReq;
 import smecalculus.bezmen.messaging.spec.SepulkaRegRes;
 import smecalculus.bezmen.validation.spec.BezmenValidator;
 
 public record SepulkaClientImpl(
-        @NonNull BezmenValidator validator, @NonNull SepulkaService service, @NonNull SepulkaConverter converter)
+        @NonNull BezmenValidator validator, @NonNull SepulkaService1 service, @NonNull SepulkaConverter converter)
         implements SepulkaClient {
 
     @Override
