4f7c476230ed7b694bc6f508719d2e2d1f6bbe9c
Pavel Vetokhin <p.vetokhin@gmail.com>
2023-10-01 16:14:28 +0300

    Fix formatting

--- libs/messaging/src/main/java/smecalculus/bezmen/configuration/MessagingProtocolPropsEg.java
+++ libs/messaging/src/main/java/smecalculus/bezmen/configuration/MessagingProtocolPropsEg.java
@@ -7,8 +7,7 @@ import java.util.Set;
 public class MessagingProtocolPropsEg {
     public static class Builders {
         public static MessagingProtocolProps.Builder messagingProtocolProps() {
-            return MessagingProtocolProps.builder()
-                    .protocolModes(Set.of(HTTP));
+            return MessagingProtocolProps.builder().protocolModes(Set.of(HTTP));
         }
     }
 }
