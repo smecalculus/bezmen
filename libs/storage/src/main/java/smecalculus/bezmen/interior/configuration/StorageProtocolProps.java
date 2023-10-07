b06682df81ad45632924a66faa5100842bb833ac
Pavel Vetokhin <p.vetokhin@gmail.com>
2023-10-07 20:18:01 +0300

    Skip install & deploy

--- libs/storage/src/main/java/smecalculus/bezmen/interior/configuration/StorageProtocolProps.java
+++ libs/storage/src/main/java/smecalculus/bezmen/interior/configuration/StorageProtocolProps.java
@@ -2,8 +2,7 @@ package smecalculus.bezmen.interior.configuration;
 
 import lombok.Builder;
 import lombok.NonNull;
-import org.springframework.lang.Nullable;
 
 @Builder
 public record StorageProtocolProps(
-        @NonNull StorageProtocolMode protocolMode, @Nullable H2Props h2Props, @Nullable PostgresProps postgresProps) {}
+        @NonNull StorageProtocolMode protocolMode, H2Props h2Props, PostgresProps postgresProps) {}
