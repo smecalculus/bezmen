b7fac62031f269b153872301874c816cfff803f2
Pavel Vetokhin <p.vetokhin@gmail.com>
2023-11-30 20:45:57 +0300

    Move testing to tools

--- libs/essentials/src/main/java/smecalculus/bezmen/validation/ValidationPropsMapper.java
+++ libs/essentials/src/main/java/smecalculus/bezmen/validation/ValidationPropsMapper.java
@@ -3,6 +3,7 @@ package smecalculus.bezmen.validation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import smecalculus.bezmen.configuration.ValidationDm;
+import smecalculus.bezmen.configuration.ValidationDm.ValidationMode;
 import smecalculus.bezmen.configuration.ValidationEm;
 import smecalculus.bezmen.mapping.EdgeMapper;
 
@@ -11,7 +12,7 @@ public interface ValidationPropsMapper extends EdgeMapper {
     @Mapping(source = "mode", target = "validationMode")
     ValidationDm.ValidationProps toDomain(ValidationEm.ValidationProps props);
 
-    default ValidationDm.ValidationMode toValidationMode(String value) {
-        return ValidationDm.ValidationMode.valueOf(value.toUpperCase());
+    default ValidationMode toValidationMode(String value) {
+        return ValidationMode.valueOf(value.toUpperCase());
     }
 }
