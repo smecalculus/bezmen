42bf03ae6b5389d15f39e98edf9b17795180c265
Pavel Vetokhin <p.vetokhin@gmail.com>
2024-02-24 16:53:38 +0300

    Fix linter issues

--- tool/testing/src/main/java/smecalculus/bezmen/testing/ShardingExtension.java
+++ tool/testing/src/main/java/smecalculus/bezmen/testing/ShardingExtension.java
@@ -3,25 +3,24 @@ package smecalculus.bezmen.testing;
 import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.extension.ConditionEvaluationResult.disabled;
 import static org.junit.jupiter.api.extension.ConditionEvaluationResult.enabled;
-import static smecalculus.bezmen.configuration.TestingProps.MODULUS_KEY;
-import static smecalculus.bezmen.configuration.TestingProps.REMINDER_KEY;
 
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
 import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import smecalculus.bezmen.configuration.TestingProps;
 
 public class ShardingExtension implements ExecutionCondition {
 
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
-        int modulus = requireNonNull(Integer.getInteger(MODULUS_KEY));
+        int modulus = requireNonNull(Integer.getInteger(TestingProps.MODULUS_KEY));
         if (modulus <= 1) {
             return enabled("without sharding");
         }
         if (context.getTestMethod().isEmpty()) {
             return enabled("classes always enabled");
         }
-        var reminder = requireNonNull(Integer.getInteger(REMINDER_KEY));
+        var reminder = requireNonNull(Integer.getInteger(TestingProps.REMINDER_KEY));
         var testId = context.getUniqueId().hashCode();
         if (testId % modulus == reminder) {
             return enabled("shard match: " + reminder);
